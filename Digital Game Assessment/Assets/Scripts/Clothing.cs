b0ed06ebac605763441d821360cae65e5bc06224
Andrew <shimeroh@gmail.com>
2016-10-07 21:38:07 +1300

    Gave each item its own home position
    This allows for the items to move all around without being viewed as
    home unless they are in one specific position

--- Digital Game Assessment/Assets/Scripts/Clothing.cs
+++ Digital Game Assessment/Assets/Scripts/Clothing.cs
@@ -5,6 +5,7 @@ public class Clothing : MonoBehaviour {
 
     public float knockback;
     public bool isCorrectPosition = true;
+    public GameObject home;
     Rigidbody2D rb;
 
 	// Use this for initialization
@@ -26,9 +27,12 @@ public class Clothing : MonoBehaviour {
                     rb.AddForce(new Vector2((transform.position.x - col.gameObject.transform.position.x), (transform.position.y - col.gameObject.transform.position.y)) * knockback * 100, ForceMode2D.Impulse);
                 }
                 break;
-            case "Safe":
+            case "Home":
                 {
-                    isCorrectPosition = true;
+                    if(col.gameObject.GetInstanceID() == home.GetInstanceID())
+                    {
+                        isCorrectPosition = true;
+                    }
                 }
                 break;
         }
@@ -38,7 +42,7 @@ public class Clothing : MonoBehaviour {
     {
         switch(col.gameObject.tag)
         {
-            case "Safe":
+            case "Home":
                 {
                     isCorrectPosition = false;
                 }
