6b33470b133540f9489784c8e36fb312e6883b86
Andrew <shimeroh@gmail.com>
2016-10-08 16:50:40 +1300

    Added a wall between "shops", along with text to show current winner

---
+++ Digital Game Assessment/Assets/Scripts/Game.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+    public Store store1;
+    public Store store2;
+    public Text winningText;
+
+	// Use this for initialization
+	void Start () {
+        SetText("Draw!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(store1.itemsAtHome >  store2.itemsAtHome)
+        {
+            SetText("Left Team");
+        }
+        else if(store1.itemsAtHome < store2.itemsAtHome)
+        {
+            SetText("Right Team");
+        }
+        else if(store1.itemsAtHome == store2.itemsAtHome)
+        {
+            SetText("Draw!");
+        }
+	}
+
+    void SetText(string winningTeam)
+    {
+        winningText.text = "Winning Team: " + winningTeam;
+    }
+}
