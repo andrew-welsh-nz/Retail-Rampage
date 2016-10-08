3626859663fe3f0576f3ae8a95082ecfe3a72bae
Andrew <shimeroh@gmail.com>
2016-10-08 20:30:10 +1300

    Added a main menu

---
+++ Digital Game Assessment/Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public string oneVsOne;
+
+    public void twoPlayer()
+    {
+        SceneManager.LoadScene(oneVsOne);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
