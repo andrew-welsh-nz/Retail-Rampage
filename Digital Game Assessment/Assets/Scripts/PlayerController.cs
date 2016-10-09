0130f62440f3a0c79e8d5f11caf5a90d48f71955
Andrew Welsh <shimeroh@gmail.com>
2016-10-10 11:33:21 +1300

    Made a nicer input detection method

--- Digital Game Assessment/Assets/Scripts/PlayerController.cs
+++ Digital Game Assessment/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public int playerNumber;
+    public string playerPrefix;
     public Store store;
     public float maxSpeed;
     public GameObject interactObject;
@@ -20,37 +20,17 @@ public class PlayerController : MonoBehaviour {
             Physics2D.IgnoreCollision(this.GetComponent<Collider2D>(), store.items[i].GetComponent<Collider2D>());
         }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        switch (playerNumber)
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxis(playerPrefix + "Horizontal") * maxSpeed, Input.GetAxis(playerPrefix + "Vertical") * maxSpeed);
+
+        if (Input.GetAxis(playerPrefix + "Interact") == 1)
         {
-            case 1:
-                {
-                    rb.velocity = new Vector2(Input.GetAxis("Horizontal") * maxSpeed, Input.GetAxis("Vertical") * maxSpeed);
-
-                    if (Input.GetAxis("Interact") == 1)
-                    {
-                        Physics2D.IgnoreCollision(interactObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
-                        interactObject.transform.position = transform.position;
-                        interactObject.SetActive(true);
-                    }
-                }
-                break;
-            case 2:
-                {
-                    rb.velocity = new Vector2(Input.GetAxis("Horizontal2") * maxSpeed, Input.GetAxis("Vertical2") * maxSpeed);
-
-                    if (Input.GetAxis("Interact2") == 1)
-                    {
-                        Physics2D.IgnoreCollision(interactObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
-                        interactObject.transform.position = transform.position;
-                        interactObject.SetActive(true);
-                    }
-                }
-                break;
-            default:
-                break;
+            Physics2D.IgnoreCollision(interactObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+            interactObject.transform.position = transform.position;
+            interactObject.SetActive(true);
         }
-	}
+    }
 }
