6619883a478f4f0ab76a0e45f8d273dfa46a11fd
Andrew <shimeroh@gmail.com>
2016-10-10 21:08:49 +1300

    Revert "Added a short stun and knockback if a player is near another when they use the interact button"
    This reverts commit 2084a2f5eb2febaf3dc4a77f3b5f3e1950436fc1.

--- Digital Game Assessment/Assets/Scripts/PlayerController.cs
+++ Digital Game Assessment/Assets/Scripts/PlayerController.cs
@@ -9,7 +9,6 @@ public class PlayerController : MonoBehaviour {
     public GameObject interactObject;
     public bool isCarrying = false;
     public GameObject pickupTrigger;
-    bool stunned = false;
     Rigidbody2D rb;
 
     // Use this for initialization
@@ -25,53 +24,22 @@ public class PlayerController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if(!stunned)
+        rb.velocity = new Vector2(Input.GetAxis(playerPrefix + "Horizontal") * maxSpeed, Input.GetAxis(playerPrefix + "Vertical") * maxSpeed);
+
+        if (Input.GetAxis(playerPrefix + "Interact") == 1)
         {
-            rb.velocity = new Vector2(Input.GetAxis(playerPrefix + "Horizontal") * maxSpeed, Input.GetAxis(playerPrefix + "Vertical") * maxSpeed);
-
-            if (Input.GetAxis(playerPrefix + "Interact") == 1)
-            {
-                Physics2D.IgnoreCollision(interactObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
-                interactObject.transform.position = transform.position;
-                interactObject.SetActive(true);
-            }
-
-            if (isCarrying)
-            {
-                pickupTrigger.SetActive(false);
-            }
-            else
-            {
-                pickupTrigger.SetActive(true);
-            }
+            Physics2D.IgnoreCollision(interactObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+            interactObject.transform.position = transform.position;
+            interactObject.SetActive(true);
         }
-    }
 
-    // STUN STUFF HAPPENS BELOW HERE
-    // CAN BE REMOVED IF WE DON'T LIKE IT
-
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        switch (col.gameObject.tag)
+        if(isCarrying)
         {
-            case "Interact":
-                {
-                    if (!stunned)
-                    {
-                        stunned = true;
-                        StartCoroutine(Stun(0.5f));
-                        rb.AddForce(new Vector2((transform.position.x - col.gameObject.transform.position.x), (transform.position.y - col.gameObject.transform.position.y)) * 1 * 100, ForceMode2D.Impulse);
-                    }
-                }
-                break;
-            default:
-                break;
+            pickupTrigger.SetActive(false);
         }
-    }
-
-    IEnumerator Stun(float _delay)
-    {
-        yield return new WaitForSeconds(_delay);
-        stunned = false;
+        else
+        {
+            pickupTrigger.SetActive(true);
+        }
     }
 }
