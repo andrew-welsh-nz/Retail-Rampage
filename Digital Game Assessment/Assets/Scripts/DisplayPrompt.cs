ebad5e9c92559c1dd4fb722ec6e9457760196ec7
Andrew <shimeroh@gmail.com>
2016-10-28 10:42:38 +1300

    Both players now have a button prompt over their heads when an interaction is possible on the menu

---
+++ Digital Game Assessment/Assets/Scripts/DisplayPrompt.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayPrompt : MonoBehaviour {
+
+    private SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () {
+        sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        switch(col.gameObject.tag)
+        {
+            case "ButtonPrompt":
+                {
+                    //sprite.gameObject.SetActive(true);
+                    sprite.enabled = true;
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        switch (col.gameObject.tag)
+        {
+            case "ButtonPrompt":
+                {
+                    sprite.enabled = false;
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+}
